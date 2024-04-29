e348fff090e11847aebad594741631c160551acf
badzso-boop <norbert.ujj@gmail.com>
2024-04-29 22:37:35 +0200

    proba

---
+++ src/HUN/Home2.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import "../App.css";
+import Footer from "./Footer";
+import { BrowserRouter as Router, Route, Link, Routes } from "react-router-dom";
+
+function Home() {
+  return (
+    <div>
+      <div className="background">
+        
+      </div>
+    </div>
+  );
+}
+
+export default Home;
