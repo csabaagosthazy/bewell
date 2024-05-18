54a3b422c9c2cbe6801713df5d79627ddd8bfda3
badzso-boop <norbert.ujj@gmail.com>
2024-05-18 09:28:10 +0200

    asd

--- magdi-frontend-de/src/DE/Magamrol.js
+++ magdi-frontend-de/src/DE/Magamrol.js
@@ -7,16 +7,8 @@ function Magamrol() {
     <div className="teljes">
       <div className="background">
         <div className="row">
-          <div className="col-md-6 col-lg-6 col-xl-6 text-center">
-            <img
-              className="m-3 rounded"
-              style={{ width: "75%" }}
-              src="images/magamrol.jpg"
-              alt="magamrol.jpg"
-            />
-          </div>
-          <div className="col-md-4 col-lg-4 col-xl-4 p-3">
-            <div class="p-2 m-auto">
+          <div className="col-md-4 col-lg-4 col-xl-4 p-3 m-auto">
+            <div class="p-2">
               <h5>Über mich</h5>
               <p>
                 Herzlich willkommen! Ich bin Magdolna Kozma und seit Jahrzehnten
@@ -37,6 +29,14 @@ function Magamrol() {
                 Gesundheit verbessern kann.
               </p>
             </div>
+          </div>
+          <div className="col-md-6 col-lg-6 col-xl-6 text-center">
+            <img
+              className="m-3 rounded"
+              style={{ width: "75%" }}
+              src="images/magamrol.jpg"
+              alt="magamrol.jpg"
+            />
           </div>
         </div>
       </div>
--- magdi-frontend/src/HUN/Magamrol.js
+++ magdi-frontend/src/HUN/Magamrol.js
@@ -7,8 +7,8 @@ function Magamrol() {
     <div className="teljes">
       <div className="background">
         <div className="row">
-          <div className="col-md-4 col-lg-4 col-xl-4 p-3">
-            <div class="p-2 m-auto">
+          <div className="col-md-4 col-lg-4 col-xl-4 p-3 m-auto">
+            <div class="p-2">
               <h5>Magamról</h5>
               <p>
                 Üdvözlöm! Kozma Magdolna vagyok, évtizedek óta az egészséges
