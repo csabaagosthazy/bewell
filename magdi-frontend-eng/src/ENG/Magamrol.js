edcc867e407571ba3784dfa9d2f9833162d2f6de
badzso-boop <norbert.ujj@gmail.com>
2024-05-18 09:10:07 +0200

    javitasok

--- magdi-frontend-eng/src/ENG/Magamrol.js
+++ magdi-frontend-eng/src/ENG/Magamrol.js
@@ -7,8 +7,8 @@ function Magamrol() {
     <div className="teljes">
       <div className="background">
         <div className="row">
-          <div className="col-md-4 col-lg-4 col-xl-4 p-3">
-            <div class="p-2 m-auto">
+          <div className="col-md-4 col-lg-4 col-xl-4 p-3 m-auto">
+            <div class="p-2">
               <h5>About Me</h5>
               <p>
                 Welcome! My name is Magdolna Kozma, and I have been a supporter
